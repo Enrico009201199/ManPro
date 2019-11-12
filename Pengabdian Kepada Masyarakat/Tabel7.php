d376f599a6427e2cd36871cde0abd9bbe372df3b
Enrico Wibawa <enricowibawa6@gmail.com>
2019-11-12 15:21:36 +0700

    Penelitian dan Pengmas

---
+++ Pengabdian Kepada Masyarakat/Tabel7.php
@@ -0,0 +1,44 @@
+<?php 
+//Connect Database
+include 'connection.php';
+include "style.html";
+
+$query = "EXEC dbo.Tabel7_PKMDTPSMahasiswa";
+$noCount = sqlsrv_query($conn,"SET NOCOUNT ON");
+$stmt = sqlsrv_query($conn,$query);
+
+// FETCH ARRAY
+//Table Header
+echo "<table>";
+echo "<tr>";
+  echo "<th>No</th>";
+  echo "<th>Nama Dosen</th>";
+  echo "<th>Tema PkM sesuai Roadmap";
+  echo "<th>Nama Mahasiswa</th>";
+  echo "<th>Judul Kegiatan</th>";
+  echo " <th>Tahun</th>";
+echo "</tr>";
+//Table Content
+//Nomor Baris
+$no = 1;
+while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+  echo "<tr>";
+    echo "<td>" . $no . "</td>";
+    echo "<td>" . $row['namaDosen'] . "</td>";
+    echo "<td>" . $row['temaPKM'] . "</td>";
+    echo "<td>" . $row['namaMahasiswa'] . "</td>";
+    echo "<td>" . $row['judulKegiatan'] . "</td>";
+    echo "<td>" . $row['tahun'] . "</td>";
+  echo "</tr>";
+  $no ++;
+}
+
+//Hitung jumlah kegiatan
+$no = $no - 1;
+echo "<tr>";
+  echo "<td colspan=4> Jumlah </td>";
+  echo "<td>" . $no . "</td>";
+  echo "<td> </td>";
+echo "</tr>";
+
+echo "</table>";

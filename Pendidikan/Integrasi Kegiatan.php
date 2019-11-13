d711896d119420423f15ebf2ef7db4a163c15d2f
Pedro <pedrovieri21@gmail.com>
2019-11-13 16:33:48 +0700

    Menambahkan file chart.bundle.js dan utils.js untuk membuat grafik Menghapus beberapa kolom dari tabel kurikulum Menghubungkan tabel kurikulum dengan database dan menampilkan data-datanya pada tabel Memperbaiki cara koneksi tabel integrasi kegiatan, sehingga menampilkan data pada tabel dengan benar Membuat diagram lingkaran untuk tabel integrasi kegiatan Membuat diagram garis untuk tabel kurikulum

--- Pendidikan/Integrasi Kegiatan.php
+++ Pendidikan/Integrasi Kegiatan.php
@@ -1,26 +1,9 @@
 <html>
     <body>
         <?php
-            include "../Header/Header.php"
+            include "../Header/Header.php";
             include "connection.php";
-            $query = "CREATE TABLE '#tempKepMahasiswa' (
-                        idInteg int,
-                        idSumber varchar(255),
-                        bentukSumber varchar(255),
-                        namaDosen varchar(255),
-                        tahun varchar(4),
-                        idMatkul int,
-                        namaMatkul varchar(255),
-                        judulPenelitian varchar(255),
-                        bentukInteg varchar(255)
-                    );
-
-                    INSERT INTO '#tempKepMahasiswa' EXEC dbo.Tabel5b_IntegrasiKegiatanPenelitianPkM
-
-                    SELECT judulPenelitian, namaDosen, namaMatkul, bentukInteg
-                    FROM '#tempKepMahasiswa'
-                    
-                    DELETE TABLE '#tempKepMahasiswa'";
+            $query = "EXEC dbo.Tabel5b_IntegrasiKegiatanPenelitianPkM";
             
             $noCount = sqlsrv_query($conn, "SET NOCOUNT ON");
             $stmt = sqlsrv_query($conn, $query);
@@ -43,10 +26,10 @@
                     while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
                         echo "<tr>";
                             echo "<td>".$number."</td>";
-                            echo "<td>".$row['judulPenelitian']."</td>";
-                            echo "<td>".$row['namaDosen']."</td>";
-                            echo "<td>".$row['namaMatkul']."</td>";
-                            echo "<td>".$row['bentukInteg']."</td>";
+                            echo "<td>".$row['judul']."</td>";
+                            echo "<td>".$row['dosen']."</td>";
+                            echo "<td>".$row['namaMatakuliah']."</td>";
+                            echo "<td>".$row['bentukIntegrasi']."</td>";
                         echo "</tr>";
                         $number++;
                     }

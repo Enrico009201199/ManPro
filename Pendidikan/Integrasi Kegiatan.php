2a88c6e5f596eb141e91b768bcdf670a9ede1832
Enrico Wibawa <enricowibawa6@gmail.com>
2019-11-15 12:16:47 +0700

    Revisi Pendidikan

--- Pendidikan/Integrasi Kegiatan.php
+++ Pendidikan/Integrasi Kegiatan.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <html>
     <body>
         <?php
@@ -43,50 +42,4 @@
             <a href="Grafik/Grafik Integrasi Kegiatan.php" target="_blank">Tampilkan Grafik Integrasi Kegiatan Penelitian/PkM Dalam Pembelajaran</a> 
         </div>
     </body>
-=======
-<html>
-    <body>
-        <?php
-            include "../Header/Header.php";
-            include "connection.php";
-            $query = "EXEC dbo.Tabel5b_IntegrasiKegiatanPenelitianPkM";
-            
-            $noCount = sqlsrv_query($conn, "SET NOCOUNT ON");
-            $stmt = sqlsrv_query($conn, $query);
-        ?>
-
-        <div class="">
-            <h3>Integrasi Kegiatan Penelitian/PkM Dalam Pembelajaran</h3>
-            
-            <div class="">
-            <table>
-                <tr>
-                    <th>No.</th>
-                    <th>Judul Penelitian/PkM</th>
-                    <th>Nama Dosen</th>
-                    <th>Mata Kuliah</th>
-                    <th>Bentuk Integrasi</th>
-                </tr>
-                <?php
-                    $number = 1;
-                    while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-                        echo "<tr>";
-                            echo "<td>".$number."</td>";
-                            echo "<td>".$row['judul']."</td>";
-                            echo "<td>".$row['dosen']."</td>";
-                            echo "<td>".$row['namaMatakuliah']."</td>";
-                            echo "<td>".$row['bentukIntegrasi']."</td>";
-                        echo "</tr>";
-                        $number++;
-                    }
-                ?>
-            </table>
-        </div>
-        </div>
-        <br>
-        <div class="">
-            <a href="Grafik/Grafik Integrasi Kegiatan.php" target="_blank">Tampilkan Grafik Integrasi Kegiatan Penelitian/PkM Dalam Pembelajaran</a> 
-        </div>
-    </body>
->>>>>>> timIntan
 </html>
